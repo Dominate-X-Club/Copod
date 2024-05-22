1e8f05fb1f12c3bf2c1193b43e8047258940271d
Abhinav Singhal <abhisinghal1087@gmail.com>
2024-05-22 12:01:11 +0000

    feat: v1 of landing page

---
+++ src/components/hero-section/hero-first.tsx
@@ -0,0 +1,18 @@
+"use client"
+import { Label } from "@radix-ui/react-label";
+import { Button } from "../ui/button";
+
+export default function HeroFirst() {
+    return <>
+        <div className="flex flex-col h-full items-start space-y-2 justify-center">
+            <Label className="text-white text-5xl">go from</Label>
+            <Label className="text-white text-7xl">zero to 1st <b>&nbsp;podcast</b></Label>
+            <Label className="text-white text-3xl">& beyond 🚀</Label>
+            <Label className="text-white text-xl pt-[40px]">with our Ready to Shoot, Hospitality Centric</Label>
+            <Label className="text-white text-xl ">Managed Podcast Studio.</Label>
+            <div className="pt-[40px]">
+                <Button>Start Podcasting, Instantly!</Button>
+            </div>
+        </div>
+    </>
+}
---
+++ src/components/waitinglist/WaitingForm.tsx
@@ -0,0 +1,34 @@
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import DatePickerDemo from "../ui/date-picker";
+
+export default function WaitingForm() {
+    return (
+        <Card className="w-svw mx-20">
+            <CardHeader>
+                <CardTitle className="text-4xl">Shoot your Podcast</CardTitle>
+                <CardDescription className="text-xl">with <b>copod</b> anyone can record a Podcast.<br/>Just invite your guests, and start talking.<br/>It's that easy!</CardDescription>
+            </CardHeader>
+            <CardContent>
+                <form>
+                    <Label>Name</Label>
+                    <Input placeholder="Abhinav Singhal" className=""></Input>
+                    <Label>Phone</Label>
+                    <Input placeholder="+91 8882021243" className=""></Input>
+                    <Label>Email</Label>
+                    <Input placeholder="contact@umun.in" className=""></Input>
+                    <Label>City</Label>
+                    <Input placeholder="Delhi 🇮🇳" className=""></Input>
+
+<Label>Recording Date</Label> <br/>
+                    <DatePickerDemo className="w-full"></DatePickerDemo>
+                </form>
+            </CardContent>
+            <CardFooter className="flex justify-between">
+                <Button className="bg-black text-main border-white shadow-base-black">Book Your Slot</Button>
+            </CardFooter>
+        </Card>
+    )
+}
