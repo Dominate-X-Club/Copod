a46baafb606508bb5392690ddc73b4281ba5633d
Bobby <bobbypatel94@gmail.com>
2024-06-14 19:00:30 +0530

    hero page responsive

--- src/components/hero-section/hero-first.tsx
+++ src/components/hero-section/hero-first.tsx
@@ -3,16 +3,18 @@ import { Label } from "@radix-ui/react-label";
 import { Button } from "../ui/button";
 
 export default function HeroFirst() {
-    return <>
-        <div className="flex flex-col h-full items-start space-y-2 justify-center">
-            <Label className="text-white text-5xl">go from</Label>
-            <Label className="text-white text-7xl">zero to 1st <b>&nbsp;podcast</b></Label>
-            <Label className="text-white text-3xl">& beyond 🚀</Label>
-            <Label className="text-white text-xl pt-[40px]">with copod&apos;s Ready to Shoot, Hospitality Centric</Label>
-            <Label className="text-white text-xl ">Managed Podcast Studio.</Label>
-            <div className="pt-[40px]">
-                <Button>Start Podcasting, Instantly!</Button>
+    return (
+        <>
+            <div className="flex flex-col h-full items-start space-y-2 justify-center">
+                <Label className="text-white text-5xl self-center lg:self-auto">go from</Label>
+                <Label className="text-white text-7xl self-center lg:self-auto text-center lg:text-auto">zero to 1st <b>&nbsp;podcast</b></Label>
+                <Label className="text-white text-3xl self-center lg:self-auto">& beyond 🚀</Label>
+                <Label className="text-white text-xl pt-[40px] self-center lg:self-auto text-center lg:text-auto ml-px mr-px lg:m-0">with copod&apos;s Ready to Shoot, Hospitality Centric</Label>
+                <Label className="text-white text-xl self-center lg:self-auto">Managed Podcast Studio.</Label>
+                <div className="pt-[40px] pb-[40px] lg:pb-0 self-center lg:self-auto">
+                    <Button>Start Podcasting, Instantly!</Button>
+                </div>
             </div>
-        </div>
-    </>
+        </>
+    );
 }
